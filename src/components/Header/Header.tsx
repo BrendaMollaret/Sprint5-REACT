9ebb74e2a870a408939292de4be3a3446194e69e
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-11-12 10:13:26 -0300

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
+import { Navbar, Container, Nav, Button } from "react-bootstrap"
 
 const Header = () => {
   return (
@@ -19,19 +19,17 @@ const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#home">Inicio</Nav.Link>
+          <Nav.Link href="#link">Locales</Nav.Link>
+          <Nav.Link href="#link">Nosotros</Nav.Link>
+          <Nav.Link href="#link">Contactanos</Nav.Link>
+        </Nav>
+        <Nav className="me-auto">
+          <Container className="">
+            <Button> Registrarse </Button>
+            <Button> Iniciar sesión </Button>
+          </Container>
+
         </Nav>
       </Navbar.Collapse>
     </Container>
